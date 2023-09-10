b17b89d58264b6bedc863706429510a2fc5a6646
Nowacki97PL <noszo997@gmail.com>
2023-09-10 13:02:34 +0200

    editing settings database

--- RentCar/settings.py
+++ RentCar/settings.py
@@ -79,13 +79,9 @@ WSGI_APPLICATION = "RentCar.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "rentcarpt4",
-        "USER": "postgres",
-        "PASSWORD": "3010Audi",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
